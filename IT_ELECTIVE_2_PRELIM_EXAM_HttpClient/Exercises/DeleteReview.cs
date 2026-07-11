17105c1ae6d2a2db2b90c24f7e1d50f5b9240ddf
Jerome arejola <jeromearejola30@gmail.com>
2026-07-11 08:44:33 +0800

    8

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/DeleteReview.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/DeleteReview.cs
@@ -13,9 +13,20 @@ public static class DeleteReview
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send DELETE request to https://jsonplaceholder.typicode.com/posts/1
-        // TODO: Assert status code is 200 OK
+        string url = "https://jsonplaceholder.typicode.com/posts/1";
 
-        throw new NotImplementedException();
+        // 1. Send a DELETE request to remove post with ID 1
+        HttpResponseMessage response = await client.DeleteAsync(url);
+
+        // 2. Assert status code is 200 OK
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        {
+            throw new Exception($"Expected status code 200 OK, but got {response.StatusCode}");
+        }
+
+        // Alternative quick assertion check built into HttpClient:
+        // response.EnsureSuccessStatusCode(); 
+
+        Console.WriteLine($"Successfully deleted post. Status Code: {response.StatusCode}");
     }
 }
