71ea5e11cbd895e52a0be6203a4a9b8f3aa743c1
Allen Dwayn T. Quimson <jeromearejola30@gmail.com>
2026-07-11 08:21:47 +0800

    Exercise 3 and 4

--- IT_ELECTIVE_2_PRELIM_EXAM/Models/Category.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM/Models/Category.cs
@@ -1,29 +1,27 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM.Models;
-
-// EXERCISE 3 & 4: Constructors - Default and Parameterized
-// The Category class has stub constructors. Your task:
-// - EXERCISE 3: Fix the default constructor to initialize Name to "" and Description to ""
-// - EXERCISE 4: Fix the parameterized constructor to assign name and description to the properties
-
-public class Category
+namespace IT_ELECTIVE_2_PRELIM_EXAM.Models
 {
-    public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
-
-    // EXERCISE 3: This constructor should initialize Name and Description to ""
-    // Currently it does nothing - fix it!
-    public Category()
+    public class Category
     {
-    }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
 
-    // EXERCISE 4: This constructor should set Name and Description from parameters
-    // Currently it does nothing - fix it!
-    public Category(string name, string description)
-    {
-    }
+        // EXERCISE 3: Default constructor
+        public Category()
+        {
+            Name = "";
+            Description = "";
+        }
 
-    public override string ToString()
-    {
-        return $"Category: {Name} - {Description}";
+        // EXERCISE 4: Parameterized constructor
+        public Category(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"Category: {Name} - {Description}";
+        }
     }
 }
